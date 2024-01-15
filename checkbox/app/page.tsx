65390d53a40f7c1804f011aa76029d75bfa9434c
RS SURYA <rssurya19@siesgst.ac.in>
2024-01-15 20:30:41 +0530

    first commit

---
+++ checkbox/app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ICheckbox from "./check/check"; 
+
+function App() {
+  return (
+    <main>
+      <h1>Welcome to Checkbox Page</h1>
+
+      <div className="row">
+        <ICheckbox/>
+      </div>
+    </main>
+  );
+}
+
+export default App;
